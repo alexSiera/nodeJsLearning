ef6143dc63dad3e7748647132a3cd61977dde3f6
alexSiera <fa1nt.siera@gmail.com>
2020-06-12 21:11:10 +0300

    start working with http module

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const userObj = require('./user');
-console.log('Hello', __dirname, __filename);
-console.log(userObj);
-console.log(userObj.user);
-userObj.sayHello();
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    res.write("<h1>hello from nodeJs</h1>");
+    res.write("<h2>hello from nodeJs</h2>");
+    res.write("<h3>hello from nodeJs</h3>");
+    res.write("<h4>hello from nodeJs</h4>");
+    res.write("<h4>hello from nodeJs</h4>");
+
+
+    res.end(`
+        <div style="background: red; width: 200px; height: 200px">
+            <h4>hello from nodeJs</h4>
+        </div>
+    `);
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
