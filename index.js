fb22ddc8654d973402493d6f04f51e305382040e
alexSiera <fa1nt.siera@gmail.com>
2020-06-12 18:34:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const userObj = require('./user');
+console.log('Hello', __dirname, __filename);
+console.log(userObj);
+console.log(userObj.user);
+userObj.sayHello();
---
+++ user.js
@@ -0,0 +1,10 @@
+const user = {
+	name: "Elena",
+	age: 25
+}
+module.exports = {
+	user,
+	sayHello () {
+		console.log('Hello');
+	}
+};
