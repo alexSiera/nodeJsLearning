ae26b5c32215349210df595feb9195f5e7a35724
alexSiera <fa1nt.siera@gmail.com>
2020-06-14 10:36:22 +0300

    add new model

--- nodeExpress/index.js
+++ nodeExpress/index.js
@@ -5,18 +5,23 @@ const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 });
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
-app.use(express.static('public'));
+app
+.engine('hbs', hbs.engine)
+.set('view engine', 'hbs')
+.set('views', 'views')
+
+app
+.use(express.static('public'))
+.use(express.urlencoded({extended: true}));
+
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 
 app
-.use(homeRoutes)
-.use(addRoutes)
-.use(coursesRoutes);
+.use('/', homeRoutes)
+.use('/add',addRoutes)
+.use('/courses',coursesRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
--- nodeExpress/routes/add.js
+++ nodeExpress/routes/add.js
@@ -1,11 +1,17 @@
 const {Router} = require('express');
 const router = Router();
-
-router.get('/add', (req, res) => {
+const Course = require('../models/course');
+router
+.get('/', (req, res) => {
     res.render('add', {
         title: 'Add course',
         isAdd: true
     });
+})
+.post('/', async (req, res) => {
+    const course = new Course(req.body.title, req.body.price, req.body.img);
+    await course.save();
+    res.redirect('/courses');
 })
 
 module.exports = router;
