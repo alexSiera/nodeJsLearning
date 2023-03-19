5cd0f4c45798468962e006eaabfb89cb7d007831
fusion-tech-alexanderPavlovtsev <alexander.pavlovtsev@fusion-tech.pro>
2023-03-19 14:59:05 +0300

    End up 14 lesson

---
+++ nodeJsFromScratch/demo/src/first_lessons/require/rings/app.js
@@ -0,0 +1,19 @@
+const a = 0;
+if (a > 0) {
+  const log = require("./chracters.js");
+  log();
+} else {
+  console.log("Меньше нуля");
+}
+
+// const { stealRing, characters } = require("./chracters.js");
+// let = myChars = characters;
+
+// myChars = stealRing(myChars, "Фродо");
+
+// for (const c of characters) {
+//   console.log(c);
+// }
+
+// const log = require("./chracters.js");
+// log();
---
+++ nodeJsFromScratch/demo/src/first_lessons/require/rings/chracters.js
@@ -0,0 +1,26 @@
+// let characters = [
+//   {
+//     name: "Фродо",
+//     hasRing: false,
+//   },
+//   {
+//     name: "Бильбо",
+//     hasRing: false,
+//   },
+// ];
+
+// const stealRing = (chracters, owner) => {
+//   return characters.map((c) => {
+//     if (c.name == owner) {
+//       c.hasRing = true;
+//     } else {
+//       c.hasRing = false;
+//     }
+//   });
+// };
+
+console.log("Загруженно characters js");
+
+module.exports = function log() {
+  console.log("KLog");
+};
